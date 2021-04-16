4e00d06504c2e43eab264fe192f26b2604fdfd96
reltaher <43248170+reltaher@users.noreply.github.com>
2021-04-16 11:58:30 -0400

    Added User Authentication & Error Handling for it

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const multer = require('multer');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash')
+
+
 const csvToDatabase = require('./public/javascripts/csvparser');
+const userRoutes = require('./routes/users')
+const { isLoggedIn } = require('./middleware');
 
 const app = express();
 
@@ -12,40 +19,68 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage});
 
 
-const Data = require('./models/datalogger')
+const Data = require('./models/datalogger');
+const User = require('./models/user');
+const LocalStrategy = require('passport-local');
+const ExpressError = require('./utils/ExpressError');
 
-mongoose.connect('mongodb://localhost:27017/datalogger', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Mongo Connection Open")
-    })
-    .catch(err => {
-        console.log("Error:")
-        console.log(err)
-    })
+mongoose.connect('mongodb://localhost:27017/datalogger', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
-app.engine('ejs', ejsMate);
-app.set('views', path.join(__dirname, 'views'));
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    name: 'session',
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(express.json());
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
-    res.render('homedashboard');
-    
-})
-
-app.post('/', upload.array('files'), async (req, res) => {
-    console.log(req.files);
-    
-    req.files.forEach(element => {
-        csvToDatabase.parseCsvToDatabase(element.buffer)
-    });
-
-    res.render('homedashboard');
-})
+    res.render('users/login');
+});
 
 app.get('/data', async (req, res) => {
     // This will hold the date
@@ -96,6 +131,30 @@ app.get('/data', async (req, res) => {
         Gas_MQ135
     });
  
+})
+
+app.get('/dashboard', isLoggedIn, (req, res) => {
+    res.render('homedashboard'); 
+})
+
+app.post('/dashboard', isLoggedIn, upload.array('files'), async (req, res) => {
+    console.log(req.files);
+    
+    req.files.forEach(element => {
+        csvToDatabase.parseCsvToDatabase(element.buffer)
+    });
+
+    res.render('homedashboard');
+})
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
